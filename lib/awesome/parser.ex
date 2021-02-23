5a44b27c509a9618358939ba80673442a6b2e094
ginkul <geenkool@yandex.ru>
2021-02-23 20:48:14 +0300

    refactore

--- lib/awesome/parser.ex
+++ lib/awesome/parser.ex
@@ -2,10 +2,6 @@ defmodule Awesome.Section do
   defstruct [:name, :description, :items]
 end
 
-defmodule Awesome.Item do
-  defstruct [:url, :stars, :days, :full_text]
-end
-
 defmodule Awesome.Parser do
   alias Awesome.Github
 
@@ -17,26 +13,40 @@ defmodule Awesome.Parser do
     |> Enum.map(fn(section) ->
       [name | section] = section
       [description | items] = section
-      items = items
-              |> Enum.filter(fn(item) -> String.contains? item, "github.com" end)
-              |> Enum.map(fn(item) ->
-                [_, url] = Regex.run(~r"\((.*?)\)", item)
-                case url |> parse_item do
-                  [days, stars] -> %Awesome.Item{url: url, stars: stars, days: days, full_text: item}
-                  [] -> nil # some links are outdated
-                end
-              end)
-              |> Enum.filter(fn(item) -> item end)
+      items = parse_items(items)
       %Awesome.Section{name: name, description: description, items: items}
     end)
   end
 
-  def parse_item(url) do
+  defp parse_items(items) do
+    items
+    |> Enum.filter(fn(item) -> String.contains? item, "github.com" end)
+    |> Enum.map(fn(item) ->
+      [_, url] = Regex.run(~r"\((.*?)\)", item) # parsing markdown url
+      case url |> parse_item do
+        [date, stars] ->
+          days = get_days_diff(date)
+          %Awesome.Item{url: url, days_from_commit: days, stars: stars, full_text: item}
+        [] -> nil # some links are outdated
+      end
+    end)
+    |> Enum.filter(fn(item) -> item end)
+  end
+
+  defp parse_item(url) do
     url
     |> URI.parse
     |> Map.get(:path)
     |> Github.get!
     |> Map.get(:body)
     |> Keyword.values
+  end
+
+  defp get_days_diff(date) do
+    date
+    |> String.split("T")
+    |> hd
+    |> Date.from_iso8601!
+    |> Date.diff(Date.utc_today)
   end
 end
